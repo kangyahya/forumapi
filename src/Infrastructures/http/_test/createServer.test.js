e69d705cf55a3bad5dcb9507687e18c9e3f7f83a
KANG YAHYA <moch.yahya95@gmail.com>
2023-11-10 11:12:53 +0700

    add say hello word test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -1,6 +1,60 @@
+// const createServer = require('../createServer');
+//
+// describe('HTTP server', () => {
+//   it('should response 404 when request unregistered route', async () => {
+//     // Arrange
+//     const server = await createServer({});
+//
+//     // Action
+//     const response = await server.inject({
+//       method: 'GET',
+//       url: '/unregisteredRoute',
+//     });
+//
+//     // Assert
+//     expect(response.statusCode).toEqual(404);
+//   });
+//
+//   it('should handle server error correctly', async () => {
+//     // Arrange
+//     const requestPayload = {
+//       username: 'dicoding',
+//       fullname: 'Dicoding Indonesia',
+//       password: 'super_secret',
+//     };
+//     const server = await createServer({}); // fake injection
+//
+//     // Action
+//     const response = await server.inject({
+//       method: 'POST',
+//       url: '/users',
+//       payload: requestPayload,
+//     });
+//
+//     // Assert
+//     const responseJson = JSON.parse(response.payload);
+//     expect(response.statusCode).toEqual(500);
+//     expect(responseJson.status).toEqual('error');
+//     expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
+//   });
+// });
+
+const pool = require('../../database/postgres/pool');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+const injections = require('../../injections');
 const createServer = require('../createServer');
 
 describe('HTTP server', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+    await AuthenticationsTableTestHelper.cleanTable();
+  });
+
   it('should response 404 when request unregistered route', async () => {
     // Arrange
     const server = await createServer({});
@@ -15,26 +69,21 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404);
   });
 
-  it('should handle server error correctly', async () => {
-    // Arrange
-    const requestPayload = {
-      username: 'dicoding',
-      fullname: 'Dicoding Indonesia',
-      password: 'super_secret',
-    };
-    const server = await createServer({}); // fake injection
-
-    // Action
-    const response = await server.inject({
-      method: 'POST',
-      url: '/users',
-      payload: requestPayload,
+  describe('when GET /', () => {
+    it('should return 200 and hello world', async () => {
+      // Arrange
+      const server = await createServer({});
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual('Hello world!');
     });
-
-    // Assert
-    const responseJson = JSON.parse(response.payload);
-    expect(response.statusCode).toEqual(500);
-    expect(responseJson.status).toEqual('error');
-    expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
   });
+
+  // Skenario testing lain ...
 });
