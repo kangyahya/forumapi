2e32a61f7902de42a10cca003c0e7a966eb88dcc
KANG YAHYA <moch.yahya95@gmail.com>
2023-11-29 16:30:55 +0700

    Add Fitur Menyukai test case

---
+++ src/Applications/use_case/_test/LikeUnlikeCommentUseCase.test.js
@@ -0,0 +1,7 @@
+const LikeRepository = require('../../../Domains/Likes/LikeRepository');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const LikeUnlikeCommentUseCase = require('../LikeUnlikeCommentUseCase');
+describe('LikeUnlikeComment', () => {
+  it('');
+});
