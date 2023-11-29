7c7a3c376b7c93ed6e0e1f461c0b99e6f0ea8498
KANG YAHYA <moch.yahya95@gmail.com>
2023-11-29 22:29:00 +0700

    Delete Feature Like

--- src/Applications/use_case/_test/LikeUnlikeCommentUseCase.test.js
+++
@@ -1,7 +0,0 @@
-const LikeRepository = require('../../../Domains/Likes/LikeRepository');
-const CommentRepository = require('../../../Domains/comments/CommentRepository');
-const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
-const LikeUnlikeCommentUseCase = require('../LikeUnlikeCommentUseCase');
-describe('LikeUnlikeComment', () => {
-  it('');
-});
